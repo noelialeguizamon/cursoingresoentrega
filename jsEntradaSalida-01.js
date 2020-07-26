514806e040d2d056734b4a4715ea0e441bfbc885
noelialeguizamon <68036315+noelialeguizamon@users.noreply.github.com>
2020-07-25 21:42:17 -0300

    correctos

---
+++ jsEntradaSalida-01.js
@@ -0,0 +1,10 @@
+/*
+Al presionar el  botón, 
+se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+function mostrar()
+{
+
+	alert("esto funciona de maravilla")
+	
+}
+
---
+++ jsEntradaSalida-03.js
@@ -0,0 +1,22 @@
+/*
+Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	var vnombre;
+
+	vnombre = document.getElementById("txtIdNombre").value;
+	alert(vnombre)
+
+
+
+
+
+
+	/*var nombreIngresado;
+	nombreIngresado=txtIdNombre.value;
+	alert("ok");*/
+
+}
+
+
